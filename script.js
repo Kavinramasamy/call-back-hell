094b115b4b30b316071776a47a5ad4d9cab0c71b
Kavinramasamy <124972388+Kavinramasamy@users.noreply.github.com>
2023-03-27 00:39:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var new_paragraph=document.createElement("p");
+new_paragraph.innerHTML="10";
+document.body.append(new_paragraph);
+
+setTimeout(()=>{
+new_paragraph.innerHTML="9";
+setTimeout(()=>{
+    new_paragraph.innerHTML="8";
+    setTimeout(()=>{
+        new_paragraph.innerHTML="7";
+        setTimeout(()=>{
+            new_paragraph.innerHTML="6";
+            setTimeout(()=>{
+                new_paragraph.innerHTML="5";
+                setTimeout(()=>{
+                    new_paragraph.innerHTML="4";
+                    setTimeout(()=>{
+                        new_paragraph.innerHTML="3";
+                        setTimeout(()=>{
+                            new_paragraph.innerHTML="2";
+                            setTimeout(()=>{
+                                new_paragraph.innerHTML="1";
+                                setTimeout(()=>{
+                                    new_paragraph.innerHTML="Happy Independence Day";
+
+
+},1000);
+        },1000);
+            },1000);
+                },1000);
+                    },1000);
+                        },1000);
+                            },1000);
+                                },1000);
+                                     },1000);
+                                        },1000);
